a1bd2e74233cd23bf0d1f8ba3aa13c2830823f8f
Aswadhpv <aswadhpv@gmail.com>
2025-10-18 19:49:11 +0700

    Completed Dashboard
    Dashboard with sidebar including all the required functions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
+import DashboardPage from './pages/DashboardPage';
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
                 <Routes>
                     <Route path="/login" element={<LoginPage />} />
                     <Route path="/signup" element={<SignUpPage />} />
+                    <Route path="/dashboard" element={<DashboardPage />} />
                     <Route path="/" element={<LoginPage />} />
                 </Routes>
             </div>
